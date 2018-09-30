533cb54e40f638f8076f83e4aaa0a5183d812ac8
FakirProyek <36882790+FakirProyek@users.noreply.github.com>
2018-09-30 23:07:27 +0700

    Initial commit

---
+++ Assets/Obstacles/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class GameManager : MonoBehaviour {
+
+
+    public GameObject obstacle1,obstacle2;
+	// Use this for initialization
+	void Start () {
+        Instantiate(obstacle1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Obstacles/Scripts/obj_1_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class obj_1_script : MonoBehaviour {
+
+
+    // Use this for initialization
+   
+    
+    void Start () {
+        
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.up*Time.deltaTime);
+	}
+}
