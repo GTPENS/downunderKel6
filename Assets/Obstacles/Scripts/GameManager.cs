e970113beeb26b2f0023620bf96f63e8400dd146
FakirProyek <36882790+FakirProyek@users.noreply.github.com>
2018-10-15 00:47:11 +0700

    update

--- Assets/Obstacles/Scripts/GameManager.cs
+++ Assets/Obstacles/Scripts/GameManager.cs
@@ -2,17 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
+
 public class GameManager : MonoBehaviour {
 
 
     public GameObject obstacle1,obstacle2;
 	// Use this for initialization
 	void Start () {
-        Instantiate(obstacle1);
+
+        
+        InvokeRepeating("initObstacle", 0, 2);
+        
+    }
+
+    private void initObstacle()
+    {
+        int id = UnityEngine.Random.Range(1,2);
+        if (id==1)
+        {
+            attachObstacle2(new Vector3(0, -5, 0));
+        }
+        else if (id==2)
+        {
+            attachObstacle1(new Vector3(0, -5, 0));
+        }
+        //switch (id)
+        //{
+        //    case 1:
+        //        attachObstacle1(new Vector3(0, -5, 0));
+        //        break;
+        //    case 2:
+        //        attachObstacle2(new Vector3(0, -5, 0));
+        //        break;
+        //    default:
+        //        break;
+        //}
+        //attachObstacle2(new Vector3(0, -5, 0));
+        //if (getNumberOfObstacle() == 4)
+        //{
+        //    CancelInvoke("initObstacle");
+        //}
+    }
+
+    // Update is called once per frame
+    void Update () {
+        updateObstacles();
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void updateObstacles()
+    {
+        for (int i = 0; i < getNumberOfObstacle(); i++)
+        {
+            getObstacle(i).gameObject.GetComponent<obj_1_script>().pubMethod();
+
+
+            if (getObstacle(i).transform.position.y >= 3)
+            {
+                removeObstacle(i);
+            }
+        }
+    }
+
+    //list untuk menampung game object obstacle
+    private List<GameObject> listOfObstacle = new List<GameObject>();
+
+    //method untuk menambahkan game object obstacle baru
+    private void attachObstacle1(Vector3 _position)
+    {
+        GameObject go = Instantiate(obstacle1);
+        go.transform.position = _position;
+        listOfObstacle.Add(go);
+        getObstacle(getNumberOfObstacle() - 1).init((int)obj_1_script.ObstacleId.obstcale1);
+    }
+
+    private void attachObstacle2(Vector3 _position)
+    {
+        GameObject go = Instantiate(obstacle2);
+        go.transform.position = _position;
+        listOfObstacle.Add(go);
+        getObstacle(getNumberOfObstacle() - 1).init((int)obj_1_script.ObstacleId.obstcale2);
+    }
+
+    private obj_1_script getObstacle(int _index)
+    {
+        return listOfObstacle[_index].gameObject.GetComponent<obj_1_script>();
+    }
+
+    private int getNumberOfObstacle()
+    {
+        return listOfObstacle.Count;
+    }
+
+    private void removeObstacle(int _index)
+    {
+        Destroy(listOfObstacle[_index].gameObject);
+        Destroy(listOfObstacle[_index]);
+        listOfObstacle.RemoveAt(_index);
+    }
 }
--- Assets/Obstacles/Scripts/obj_1_script.cs
+++ Assets/Obstacles/Scripts/obj_1_script.cs
@@ -2,21 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class obj_1_script : MonoBehaviour {
 
 
     // Use this for initialization
-   
-    
-    void Start () {
-        
-        
-        
+    private int id;
+    public static int ID_OBS_1 = 1;
+
+    public enum ObstacleId
+    {
+        obstcale1 = 1,
+        obstcale2 = 2,
+        obstcale3 = 3
     }
 
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public void init(int _id)
+    {
+        Id = _id;
+    }
+
+
+
     // Update is called once per frame
-    void Update () {
-        transform.Translate(Vector3.up*Time.deltaTime);
+    Vector3 mouseDown = new Vector3();
+    Vector3 mouseUP = new Vector3();
+    private void OnMouseDown()
+    {
+        mouseDown = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (id==3)
+        {
+            this.transform.position = new Vector3(10,transform.position.y,transform.position.z);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        mouseUP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        move(newPos);
+    }
+
+    private void move(Vector3 newPos)
+    {
+        switch (Id)
+        {
+            case 1:
+                moveType1(newPos);
+                break;
+            case 2:
+                moveType2(newPos);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void moveType1(Vector3 newPos)
+    {
+
+        if (mouseDown.x - newPos.x < 0)
+        {
+            transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void moveType2(Vector3 newPos)
+    {
+
+        if (mouseDown.x - newPos.x > 0)
+        {
+            transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+        }
+    }
+
+
+
+    public void pubMethod () {
+        //Debug.Log("sada");
+        gameObject.transform.Translate(Vector3.up * Time.deltaTime);
 	}
+    
 }
