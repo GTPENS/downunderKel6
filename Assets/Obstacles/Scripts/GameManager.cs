ef5d223ed8117c3e22de7a00d95461a21a9a6fab
FakirProyek <36882790+FakirProyek@users.noreply.github.com>
2018-10-21 23:38:51 +0700

    update 10-21-2018

--- Assets/Obstacles/Scripts/GameManager.cs
+++ Assets/Obstacles/Scripts/GameManager.cs
@@ -7,42 +7,41 @@ using System;
 public class GameManager : MonoBehaviour {
 
 
-    public GameObject obstacle1,obstacle2;
+    public GameObject obstacle1,obstacle2,obstacle3,obstacle4,obstacle5;
 	// Use this for initialization
 	void Start () {
 
         
-        InvokeRepeating("initObstacle", 0, 2);
+        InvokeRepeating("initObstacle", 0, 15);
         
     }
 
     private void initObstacle()
     {
-        int id = UnityEngine.Random.Range(1,2);
+        int id = 4;
+        
         if (id==1)
-        {
-            attachObstacle2(new Vector3(0, -5, 0));
-        }
-        else if (id==2)
         {
             attachObstacle1(new Vector3(0, -5, 0));
         }
-        //switch (id)
-        //{
-        //    case 1:
-        //        attachObstacle1(new Vector3(0, -5, 0));
-        //        break;
-        //    case 2:
-        //        attachObstacle2(new Vector3(0, -5, 0));
-        //        break;
-        //    default:
-        //        break;
-        //}
-        //attachObstacle2(new Vector3(0, -5, 0));
-        //if (getNumberOfObstacle() == 4)
-        //{
-        //    CancelInvoke("initObstacle");
-        //}
+        else if (id==2)
+        {
+            attachObstacle2(new Vector3(0, -5, 0));
+        }
+        else if (id==3)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                attachObstacle3(new Vector3(UnityEngine.Random.Range(-3f, 3f), -5, 0));
+            }
+          
+        }
+        else if (id == 4)
+        {
+            attachObstacle4(new Vector3(3.28f, -5, 0));
+        }
+        Debug.Log("id =" + id);
+        
     }
 
     // Update is called once per frame
@@ -53,10 +52,12 @@ public class GameManager : MonoBehaviour {
 
     private void updateObstacles()
     {
+
+
         for (int i = 0; i < getNumberOfObstacle(); i++)
         {
             getObstacle(i).gameObject.GetComponent<obj_1_script>().pubMethod();
-
+            
 
             if (getObstacle(i).transform.position.y >= 3)
             {
@@ -83,6 +84,24 @@ public class GameManager : MonoBehaviour {
         go.transform.position = _position;
         listOfObstacle.Add(go);
         getObstacle(getNumberOfObstacle() - 1).init((int)obj_1_script.ObstacleId.obstcale2);
+    }
+
+    private void attachObstacle3(Vector3 _position)
+    {
+
+        GameObject go = Instantiate(obstacle3);
+        go.transform.position = _position;
+        listOfObstacle.Add(go);
+        getObstacle(getNumberOfObstacle() - 1).init((int)obj_1_script.ObstacleId.obstcale3);
+    }
+
+    private void attachObstacle4(Vector3 _position)
+    {
+
+        GameObject go = Instantiate(obstacle4);
+        go.transform.position = _position;
+        listOfObstacle.Add(go);
+        getObstacle(getNumberOfObstacle() - 1).init((int)obj_1_script.ObstacleId.obstacle4);
     }
 
     private obj_1_script getObstacle(int _index)
--- Assets/Obstacles/Scripts/obj_1_script.cs
+++ Assets/Obstacles/Scripts/obj_1_script.cs
@@ -15,7 +15,9 @@ public class obj_1_script : MonoBehaviour {
     {
         obstcale1 = 1,
         obstcale2 = 2,
-        obstcale3 = 3
+        obstcale3 = 3,
+        obstacle4 = 4,
+        obstacle5 = 5
     }
 
     public int Id
@@ -53,6 +55,7 @@ public class obj_1_script : MonoBehaviour {
     private void OnMouseUp()
     {
         mouseUP = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        moveZ = mouseRadZ;
     }
 
     private void OnMouseDrag()
@@ -71,6 +74,9 @@ public class obj_1_script : MonoBehaviour {
             case 2:
                 moveType2(newPos);
                 break;
+            case 4:
+                moveType4(newPos);
+                break;
             default:
                 break;
         }
@@ -78,10 +84,16 @@ public class obj_1_script : MonoBehaviour {
 
     private void moveType1(Vector3 newPos)
     {
-
         if (mouseDown.x - newPos.x < 0)
         {
-            transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+            if(transform.position.x > 0)
+            {
+                transform.position = new Vector3(mouseUP.x + newPos.x, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+            }
         }
     }
 
@@ -90,15 +102,62 @@ public class obj_1_script : MonoBehaviour {
 
         if (mouseDown.x - newPos.x > 0)
         {
-            transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+            if(transform.position.x < 0)
+            {
+                transform.position = new Vector3(newPos.x + mouseUP.x, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.position = new Vector3(newPos.x, transform.position.y, transform.position.z);
+            }
+            
         }
     }
 
+    float moveZ = 0;
+    float mouseRadZ;
+    private void moveType4(Vector3 newPos)
+    {
+        if (transform.rotation.z < 0 || transform.rotation.z > Quaternion.Euler(0, 0, 90).z)
+        {
+            return;
+        }
+        else
+        {
+            if (moveZ > 0)
+            {
+                mouseRadZ = (mouseDown.y - newPos.y) + moveZ;
+                Debug.Log("up       " + mouseRadZ);
+                transform.rotation = Quaternion.Euler(0, 0, mouseRadZ * 10);
+            }
+            else
+            {
+                mouseRadZ = mouseDown.y - newPos.y;
+                Debug.Log("down     "+mouseRadZ);
+                transform.rotation = Quaternion.Euler(0, 0, mouseRadZ * 10);
+            }
+            
+        }
+        
+    }
 
+    private void moveType5(Vector3 newPos)
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("game Over");
+        }
+    }
 
     public void pubMethod () {
         //Debug.Log("sada");
-        gameObject.transform.Translate(Vector3.up * Time.deltaTime);
-	}
+        gameObject.transform.Translate(Vector3.up * Time.deltaTime * 0.5f,Camera.main.transform);
+        //transform.position = Vector3.MoveTowards(transform.position,new Vector3(0,5,0),Time.deltaTime);
+    }
     
 }
